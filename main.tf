2b8283cace8fafb810723bd5003785527cf98a85
Michael Gardner <mikegardner2087@gmail.com>
2025-10-31 11:47:33 -0700

    ip address ref fix

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "google_compute_firewall" "default-ilb-fw" {
     ports    = var.port_range
   }
   direction               = "INGRESS"
-  source_ranges           = local.all_proxy_ips
+  source_ranges           = [for region in var.regions : data.google_compute_subnetwork.proxy_only[region.key].ip_cidr_range]
   source_tags             = var.source_tags
   source_service_accounts = var.source_service_accounts
   target_tags             = var.target_tags
@@ -184,7 +184,7 @@ resource "google_dns_record_set" "geo" {
           for_each = var.regions
           iterator = region
           content {
-            ip_address         = data.google_compute_subnetwork.proxy_only[region.key].ip_address
+            ip_address         = google_compute_forwarding_rule.default[region.key].ip_address
             ip_protocol        = "tcp"
             load_balancer_type = "globalL7ilb"
             network_url        = data.google_compute_network.network.self_link
